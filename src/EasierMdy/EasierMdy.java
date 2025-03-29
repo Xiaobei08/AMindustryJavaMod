2fa15aafde86eabf92ead3f82c2964b95e846cab
Xiaobei <89635391+xiaobei08@users.noreply.github.com>
2025-03-29 15:13:37 +0800

    0.1
    Signed-off-by: 071205080112 <3183906066@qq.com>

---
+++ src/EasierMdy/EasierMdy.java
@@ -0,0 +1,14 @@
+package EasierMdy;
+
+import mindustry.content.Items;
+import mindustry.mod.Mod;
+
+public class EasierMdy extends Mod {
+    public static Items Items;
+    public EasierMdy(){
+    }
+    @Override
+    public void loadContent() {
+        super.loadContent();
+    }
+}
---
+++ src/EasierMdy/ModItems.java
@@ -0,0 +1,9 @@
+package EasierMdy;
+
+import mindustry.type.Item;
+public class ModItems {
+    public static Item thulium;
+    public void load(){
+        thulium = new Item("thulium");
+    }
+}
